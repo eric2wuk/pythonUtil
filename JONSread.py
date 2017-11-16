41cadd7ee5fccd1e124f9c32ef576d743ca793e2
Eric <sunyinlong@youche.ocm>
2017-11-16 17:38:42 +0800

    序列化

---
+++ JONSread.py
@@ -0,0 +1,4 @@
+import json
+fff = open('dump.txt','r')
+jjj = json.load(fff)
+print('json:',jjj)
--- myPickling.py
+++ myPickling.py
@@ -1,5 +1,5 @@
-import pickle
+import json
 d = dict(name='Bob', age=20, score=88)
-fff = open('dump.txt','wb')
-pickle.dump(d,fff)
+fff = open('dump.txt','w')
+json.dump(d,fff)
 fff.close()

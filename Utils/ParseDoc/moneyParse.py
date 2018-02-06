05592833918efe7467d48b552498a088d6f96b1b
Eric <dozesun@126.com>
2018-02-06 15:28:57 +0800

    moneyParse

---
+++ Utils/ParseDoc/moneyParse.py
@@ -0,0 +1,12 @@
+resultList = None
+line1 = None;
+line2 = None;
+dict = {};
+
+with open('money.md', 'r', encoding='utf-8') as f:
+    resultList = f.readlines();
+    line1 = resultList[0].split();
+    line2 = resultList[1].split();
+    for i in range(len(line1)):
+        dict[line1[i]]=line2[i];
+    print(dict)

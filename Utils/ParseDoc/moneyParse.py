fe63b7d8246cdfd92ea6f17da9a8840a26db25fb
Eric <dozesun@126.com>
2018-02-06 15:57:21 +0800

    money

--- Utils/ParseDoc/moneyParse.py
+++ Utils/ParseDoc/moneyParse.py
@@ -8,5 +8,6 @@ with open('money.md', 'r', encoding='utf-8') as f:
     line1 = resultList[0].split();
     line2 = resultList[1].split();
     for i in range(len(line1)):
-        dict[line1[i]]=line2[i];
+        if line2[i] != '0':
+            dict[line1[i]]=line2[i];
     print(dict)

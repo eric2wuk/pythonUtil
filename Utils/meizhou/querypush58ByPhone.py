504a739aeb0c4a4e483858877ec0062aacc66500
Eric <eric2wuk@gmail.com>
2018-10-09 16:56:10 +0800

    sql util querypush 58

---
+++ Utils/meizhou/querypush58ByPhone.py
@@ -0,0 +1,17 @@
+import requests
+import hashlib
+import sys
+
+paramid = "3"
+token = "ass"
+print(sys.argv)
+param = sys.argv[1]
+clientid = "1"
+str =clientid+param+paramid+token
+hl = hashlib.md5()
+hl.update(str.encode(encoding='utf-8'))
+print('MD5加密后为 ：' + hl.hexdigest())
+key = hl.hexdigest()
+url = "Https://meizhouapi.58.com/v3/query/chaboshi/push/"+param+"/"+paramid+"?clientid=1"+"&key="+key
+r = requests.get(url)
+print(r.text)
